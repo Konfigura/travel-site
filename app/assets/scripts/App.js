173c224caf89e8aa53d50aea5985d15b6df0730d
Krystian <krystian@konfigura.studio>
2022-10-17 22:53:42 +0200

    Added some fancy JavaScript code lines.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,22 @@
 import '../styles/styles.css'
+import Person from './modules/Person'
 
 if (module.hot) {
   module.hot.accept()
 }
+
+/* Lesson example code */
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes zero taxes.");
+  }
+}
+
+
+var john = new Person("John Doe", "green");
+john.greet();
+
+var jane = new Adult("Jane Smith", "purple");
+jane.greet();
+jane.payTaxes();
